f664604362926e1bea5196061c07132e34f81c8a
amine kammoun <aminekammoun55@gmail.com>
2021-07-09 11:23:05 +0100

    add actions to store

--- src/store/store.js
+++ src/store/store.js
@@ -1,13 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
-
+import axios from "axios";
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
   state: {
     token: "",
     username: "",
-    userData:[]
+    userData: [],
+    id: "",
   },
   mutations: {},
+  actions: {
+    deleteItem(state,{id}) {
+      axios.delete("http://localhost:5000/building/" + id).then(() => {
+        console.log("hello dispatch");
+      });
+    },
+  },
 });
