f1b4f8f31b698f8fdfa487fdebe55ad13ba1d260
amine kammoun <aminekammoun55@gmail.com>
2021-07-09 13:24:24 +0100

    store firebase add dispatch

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import Vuex from "vuex";
 import axios from "axios";
+import firebase from "../db/firebase";
+console.log(firebase);
+
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
@@ -12,10 +15,15 @@ export const store = new Vuex.Store({
   },
   mutations: {},
   actions: {
-    deleteItem(state,{id}) {
+    deleteItem(state, { id }) {
       axios.delete("http://localhost:5000/building/" + id).then(() => {
         console.log("hello dispatch");
       });
+    },
+    getVote(state, { data }) {
+      var voteRef = firebase.database().ref("vote/" + data.idProgram);
+      voteRef.push(data);
+      console.log("hello noo");
     },
   },
 });
