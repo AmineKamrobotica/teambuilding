fb71048c6aac5a76879d64f3766427a20589ceb8
amine kammoun <aminekammoun55@gmail.com>
2021-07-09 13:23:59 +0100

    firebase add

---
+++ src/db/config.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyAaSWUPUq7gEAwLnEs0DzEGd7NUkvZN3dg",
+  authDomain: "teambuilding-9a65e.firebaseapp.com",
+  databaseURL:
+    "https://teambuilding-9a65e-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "teambuilding-9a65e",
+  storageBucket: "teambuilding-9a65e.appspot.com",
+  messagingSenderId: "185361470225",
+  appId: "1:185361470225:web:a9bf1420a69eb5119bf2b0",
+};
+export default config;
