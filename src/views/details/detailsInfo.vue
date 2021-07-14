33b25eadbbc0e08d83aa7bfffcc7708ed3136cca
amine kammoun <aminekammoun55@gmail.com>
2021-07-14 14:47:55 +0100

    detail info compo

---
+++ src/views/details/detailsInfo.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-row>
+    <v-col md="5" sm="6" xs="12"
+      ><h2>description :</h2>
+      <p>
+        {{ this.infos.description }}
+      </p></v-col
+    >
+    <v-col md="3" sm="6" xs="12"
+      ><h2>date :</h2>
+      <span>{{ this.infos.date }}</span></v-col
+    >
+    <v-col md="3" sm="6" xs="12"
+      ><h2>time :</h2>
+      <span>{{ this.infos.time }}</span></v-col
+    >
+    <v-col md="1" sm="6" xs="12"
+      ><h3>vote :</h3>
+      <span>{{ this.infos.vote }}</span></v-col
+    >
+  </v-row>
+</template>
+<script>
+export default {
+  props: { infos: [] },
+};
+</script>
