4f3e2721e01bd232befe3142502b0b0522db1b92
amine kammoun <aminekammoun55@gmail.com>
2021-07-16 15:49:09 +0100

    navigation drawer 15/7/2021

--- src/views/bar/navigationDrawer.vue
+++ src/views/bar/navigationDrawer.vue
@@ -1,10 +1,10 @@
 <template>
   <v-navigation-drawer v-model="this.$store.state.drawer" absolute temporary>
     <v-list nav dense>
-      <v-list-item-group active-class="deep-purple--text text--accent-4">
+      <v-list-item-group active-class="light-blue darken-4">
         <v-list-item>
           <v-list-item-title>
-            <router-link class="linkDrawer" to="/home">home</router-link>
+            <router-link class="linkDrawer" to="/">home</router-link>
           </v-list-item-title>
         </v-list-item>
         <div v-if="!this.$store.state.token && !this.$store.state.authed">
@@ -30,6 +30,15 @@
               </router-link></v-list-item-title
             >
           </v-list-item>
+          <v-btn
+            style="margin-left:10px"
+            small
+            icon
+            color="#e63946"
+            @click="logOut"
+          >
+            <v-icon>mdi-logout</v-icon>
+          </v-btn>
         </div>
       </v-list-item-group>
     </v-list>
@@ -37,11 +46,12 @@
 </template>
 <script>
 export default {
-  data: () => ({
-   
-  }),
+  data: () => ({}),
   methods: {
-    onClickOutside() {},
+    logOut() {
+      localStorage.removeItem("id");
+      this.$router.go(0);
+    },
   },
 };
 </script>
