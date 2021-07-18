7d7264454695a411e28f16399870cc9cfd14542b
amine kammoun <aminekammoun55@gmail.com>
2021-07-18 17:41:47 +0100

    beforeAuth 18/7/2021

---
+++ src/views/bar/beforeAuth.vue
@@ -0,0 +1,14 @@
+<template>
+  <span
+    style="display: flex;"
+  >
+    <router-link
+      v-for="(link, index) in this.$store.state.links"
+      :key="index"
+      class="link"
+      :to="link.link"
+    >
+      {{ link.name }}
+    </router-link>
+  </span>
+</template>
