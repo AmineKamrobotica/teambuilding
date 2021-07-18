bf5ee5cb9ccf2be8872c825870effd0c4bf5694e
amine kammoun <aminekammoun55@gmail.com>
2021-07-18 17:41:56 +0100

    mainBar 18/7/2021

---
+++ src/views/bar/mainBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <span
+    style="display: flex;"
+    
+  >
+    <router-link to="/" class="link" style="">home </router-link>
+
+    <router-link
+      v-for="(link, index) in this.$store.state.afterauth"
+      :key="index + 100"
+      class="link"
+      :to="link.link"
+    >
+      {{ link.name }}
+    </router-link>
+    <v-badge
+      :content="this.$store.state.notifications"
+      :value="this.$store.state.notifications"
+      color="green"
+      overlap
+    >
+      <v-icon>
+        mdi-bell-outline
+      </v-icon>
+    </v-badge>
+  </span>
+</template>
