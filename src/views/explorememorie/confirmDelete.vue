31249631e485d401607422a4b4cca812dd4fa2e5
amine kammoun <aminekammoun55@gmail.com>
2021-07-26 15:52:11 +0100

    confirm delete kais said for the win

---
+++ src/views/explorememorie/confirmDelete.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-row justify="center">
+    <v-dialog persistent max-width="290" v-model="dialog">
+      <v-card>
+        <v-card-title class="text-h5">
+          Warning
+        </v-card-title>
+        <v-card-text>Are you sure you want to delete this program</v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="#e63946" text @click="disagree">
+            no
+          </v-btn>
+          <v-btn color="#16db93" text @click="agree">
+            yes
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-row>
+</template>
+<script>
+export default {
+  props: {
+    dialog: Boolean,
+  },
+  methods: {
+    disagree() {
+      this.$emit("disagree");
+    },
+    agree() {
+      this.$emit("agree");
+    },
+  },
+};
+</script>
