ed27afdd8f46314ba60e2103957ee52df6af5719
amine kammoun <aminekammoun55@gmail.com>
2021-07-26 16:22:00 +0100

    comment view kais said for the win 2

---
+++ src/views/detailsMemories/comments.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <v-subheader>comments</v-subheader>
+
+    <v-list two-line>
+      <template v-for="(comment, n) in comments">
+        <v-list-item :key="n">
+          <v-list-item-avatar color="grey darken-1"> </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ comment.owner }}</v-list-item-title>
+
+            <v-list-item-subtitle>
+              {{ comment.content }}
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider
+          v-if="n !== comments.length - 1"
+          :key="`divider-${n}`"
+          inset
+        ></v-divider>
+      </template>
+    </v-list>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    comments: [],
+  },
+  data: () => ({
+    cards: ["Today", "Yesterday"],
+  }),
+};
+</script>
