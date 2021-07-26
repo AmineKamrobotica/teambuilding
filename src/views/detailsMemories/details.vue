93aa2d82dca53b35ddb19b3c82e864d7a85dc491
amine kammoun <aminekammoun55@gmail.com>
2021-07-26 16:22:15 +0100

    details view kais said for the win 2

---
+++ src/views/detailsMemories/details.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-main>
+    <v-container grid-list-xs>
+      <div class="divStyle">
+        <h2>best memories are here</h2>
+        <v-btn color="#0db39e" icon dark>
+          <v-icon> mdi-chevron-down</v-icon>
+        </v-btn>
+      </div>
+      <carousel :items="items"></carousel>
+      <details-infos :infos="detailsData"></details-infos>
+
+      <v-divider></v-divider>
+      <h3 style="margin-left:10px">comments</h3>
+      <v-divider></v-divider>
+      <v-card outlined style="margin-top: 10px"> 
+        <comments-area @clicked="comments"></comments-area>
+        <comments :comments="commentsOfThePost"></comments>
+      </v-card>
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+import axios from "axios";
+import carousel from "../carousel/Carousel.vue";
+import Comments from "./comments.vue";
+import DetailsInfos from "./detailsInfo.vue";
+import CommentsArea from "./CommentsArea.vue";
+export default {
+  props: {},
+  components: {
+    carousel,
+    DetailsInfos,
+    Comments,
+
+    CommentsArea,
+  },
+  data: () => ({
+    detailsData: [],
+    items: [],
+    comment: "",
+    commentsOfThePost: [],
+  }),
+  created() {
+    this.detailsData = this.$route.params.details;
+
+    this.commentsOfThePost = this.detailsData.comments;
+
+    for (let i = 0; i < this.detailsData.image.length; i++) {
+      this.items.push(
+        require("../../assets/images/upload/" + this.detailsData.image[i])
+      );
+    }
+
+    setInterval(() => {
+      this.getComments();
+    }, 1000);
+  },
+  methods: {
+    comments(value) {
+      if (this.$store.state.token && localStorage.getItem("id")) {
+        var comentInfo = {
+          id: this.detailsData._id,
+          content: value,
+          idPost: this.detailsData._id,
+          owner: this.$store.state.username,
+          idOwner: localStorage.getItem("id"),
+        };
+        this.$store.dispatch("postCommentMemorie", { commentInfo: comentInfo });
+      } else {
+        this.$router.push("/login");
+      }
+    },
+    getComments() {
+      axios.get("memories/" + this.detailsData._id).then((res) => {
+        this.commentsOfThePost = res.data.comments;
+      });
+    },
+  },
+};
+</script>
+<style scoped>
+@import url("./details.css");
+</style>
