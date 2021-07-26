a6aca0ac15ed58cd0f069c069bb9be8a8b73c4fa
amine kammoun <aminekammoun55@gmail.com>
2021-07-26 15:52:57 +0100

    input file kais said for the win

---
+++ src/views/memories/inputFile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    style="    display: flex;
+    margin-bottom: 15px;"
+  >
+    <v-file-input
+      v-model="files"
+      placeholder="Upload your documents"
+      label="File input"
+      multiple
+      prepend-icon="mdi-paperclip"
+      @change="chnge"
+    >
+      <template v-slot:selection="{ text }">
+        <v-chip small label color="primary">
+          {{ text }}
+        </v-chip>
+      </template>
+    </v-file-input>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    files: [],
+
+  }),
+  methods: {
+    chnge() {
+      console.log(this.files);
+      this.$emit("inputFile", this.files);
+    },
+  },
+};
+</script>
+<style scoped>
+.organizeBtn {
+  color: white;
+  text-transform: capitalize;
+}
+</style>
